bffaf552cdc124eafe6d67d3aba0ae6d415b12b7
PRussell961 <Prussel961@gmail.com>
2023-11-28 16:08:16 -0500

    not sure how this happened

--- Index.js
+++ Index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = require('express')();
-const PORT = 8080;
+const PORT = 80;
 const mysql = require("mysql");
 var cors = require('cors')
 
@@ -9,45 +9,58 @@ app.use(cors());
 app.use( express.json() );
 app.use(bodyParser.urlencoded({extended: true}));
 app.listen(PORT);
-
+//DBConnection manager
 const con = mysql.createConnection({
-    host: "database-2.cnpuqcfam1ux.us-east-2.rds.amazonaws.com",
+    host: "database-1.cnpuqcfam1ux.us-east-2.rds.amazonaws.com",
     user: "admin",
     password: "password",
-    database: "Grades"
-    
+    database: "Grades"    
   })
   con.connect(function(err) {
 	if (err) throw err}
 );
 
-function Insert(id, grade) {
-        sql = "INSERT INTO Grades (StudentID, Grades) VALUES ('" + id + "', " + grade + ");";
+/* API Requests */ 
+    //Put Request
+    app.put('/create', (req, res) => { 
+        const  StudentID  = req.body.id;
+        const  Grade  = req.body.grade;        
+        sql = "INSERT INTO Grades (StudentID, Grade) VALUES ('" + StudentID + "', " + Grade + ");";
         con.query(sql, function (err, result) {
           if (err) throw err;
           console.log("1 record inserted");
-      });
-  }
-  
-app.post('/insert', (req, res) => { 
-        const  StudentID  = req.body.id;
-        const  Grade  = req.body.grade;        
-        Insert(StudentID, Grade);        
-})
+        });
+        res.send("Insert finished");
+    })
 
-app.get('/get', (req, res) => {
+    //Get Request
+    app.get('/read', (req, res) => {
     const selectAll =
     "SELECT * FROM Grades;"
     con.query(selectAll,(err,result)=> {
         console.log("Querying for all");
         res.send(result);
     });
-})
-app.get('/getavrg', (req, res) => {
-    const selectAvrg =
-    "SELECT round(AVG(Grades),2) AS Avrg FROM Grades;"
-    con.query(selectAvrg,(err,result)=> {
-        console.log("Querying for avrg");
-        res.send(result);
-    });
-})
+    })
+    //Post Request
+    app.post('/update', (req, res) => { 
+        const  StudentID  = req.body.id;
+        const  NewGrade  = req.body.grade;
+        sql = "UPDATE Grades SET Grade ='" + NewGrade + "'WHERE StudentID='" + StudentID +"';";
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("1 Record Updated");
+        });  
+        res.send("Update finished")  
+    })
+    //Delete Request
+    app.delete('/delete', (req, res) => {
+        const  StudentID  = req.body.id;
+        sql = "DELETE FROM Grades WHERE StudentID='"+ StudentID+"';";
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("1 Record Deleted");
+        });        
+        res.send("Delete finished"); 
+    })
+
